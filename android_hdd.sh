3613f4ac50ac1477905a73b6fe7583c0a672e72c
assassin1991 <beastboy1991@gmail.com>
2018-04-05 11:11:44 +0530

    edits zero to random

--- android_hdd.sh
+++ android_hdd.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 su swapoff -a
 i=0
-size=`expr $1 \* 1048576`
+size=`expr $1 \* 1073741824`
 echo $size
 
 while true
 do
     echo $i
     sync
-    dd if=/dev/zero of=/sdcard/tempfile bs=1M count=$size
+    dd if=/dev/urandom of=/sdcard/tempfile bs=1024 count=$size
     sync
     su rm -r /data/dalvik-cache
     su rm -r /cache/dalvik-cache
